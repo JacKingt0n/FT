26d29572321292227e45426898c925932a646ce7
JacKington <85676474+JacKingt0n@users.noreply.github.com>
2024-08-29 16:37:12 -0700

    Update governance.test.js

--- FT-Platform/test/governance.test.js
+++ FT-Platform/test/governance.test.js
@@ -1,10 +1,17 @@
 const FTGovernance = artifacts.require("FTGovernance");
 
 contract("FTGovernance", (accounts) => {
-  it("should create a new proposal", async () => {
+  it("should prevent voting on executed proposals", async () => {
     const governanceInstance = await FTGovernance.deployed();
-    await governanceInstance.createProposal("Test Proposal", { from: accounts[0] });
-    const proposal = await governanceInstance.proposals(0);
-    assert.equal(proposal.description, "Test Proposal", "Proposal description mismatch");
+    await governanceInstance.createProposal("Test Proposal", 300, { from: accounts[0] });
+    await governanceInstance.voteOnProposal(0, { from: accounts[0] });
+    await governanceInstance.executeProposal(0, { from: accounts[0] });
+
+    try {
+      await governanceInstance.voteOnProposal(0, { from: accounts[1] });
+      assert.fail("Vote should not be allowed on executed proposal");
+    } catch (error) {
+      assert.include(error.message, "Proposal already executed");
+    }
   });
 });
