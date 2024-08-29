ecf9868e34181e701b3739f71a0dbcbf5736d9be
JacKington <85676474+JacKingt0n@users.noreply.github.com>
2024-08-29 16:08:08 -0700

    Create App.js

---
+++ FT-Platform/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to FT Platform</h1>
+            <p>Decentralized and Immutable Social Media Platform</p>
+        </div>
+    );
+}
+
+export default App;
